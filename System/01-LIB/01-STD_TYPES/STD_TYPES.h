65c9680e8384fdbbfd3f33f2238b521b5e74362f
Moustafa_ElGafry <mostafa25666@gmail.com>
2020-08-15 15:27:47 +0200

    Format STD_TYPES.h

--- System/01-LIB/01-STD_TYPES/STD_TYPES.h
+++ System/01-LIB/01-STD_TYPES/STD_TYPES.h
@@ -6,8 +6,8 @@
 
 #ifndef STD_TYPES_H
 #define STD_TYPES_H
-typedef unsigned char u8;
-typedef unsigned short int  u16;
-typedef unsigned long int u32;
+typedef unsigned char         u8;
+typedef unsigned short int    u16;
+typedef unsigned long int     u32;
 
 #endif
