6d5d12d8901434335f8c2ea31ede1d2e372ba7d5
Moustafa_ElGafry <mostafa25666@gmail.com>
2020-08-24 17:49:24 +0200

    Create version 1 from RCC driver

---
+++ System/02-MCAL/01-RCC/RCC_interface.h
@@ -0,0 +1,14 @@
+/*
+*Author     : Moustafa ElGafry
+*Date       : 14 AUG 2020
+*Version    : V01
+*/
+
+#ifndef RCC_INTERFACE_H
+#define RCC_INTERFACE_H
+
+#define     RCC_AHB  0
+#define     RCC_APB1 1
+#define     RCC_APB2 2
+
+#endif
